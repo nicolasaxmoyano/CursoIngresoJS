d090e7dbf19d98a47dfa349ee839467146ea5a94
nicolas moyano <nicolasaxmoyano@gmail.com>
2020-07-13 22:23:36 -0300

    Realice los ejercicios del 1 al 6 de E/S

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,8 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var dato; // camelcase (variables en minuscula)
+	dato = prompt("Ingrese un nombre:");
+    alert(dato);
 }
 
